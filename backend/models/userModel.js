d9be424b4d82a416f3040dc95c3af1a98564d116
CKDEV_MAC2012 <9k.chainarong@gmail.com>
2022-04-28 21:03:03 +0700

    insert db

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -2,23 +2,32 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  name: {
+  username: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+  },
+  password: {
     type: String,
     required: true,
   },
   email: {
     type: String,
     required: true,
-    unique: true,
   },
   gender: {
     type: String,
     required: true,
   },
   phone: {
-    type: Number,
+    type: String,
     required: true,
-    unique: true,
+    max: 10,
+  },
+  date: {
+    type: Date,
+    default: Date.now(),
   },
 });
 
--- backend/services/api.js
+++ backend/services/api.js
@@ -1,8 +1,23 @@
 const UserDB = require("../models/userModel");
 
-const createUser = (req, res) => {
-  console.log(req.body);
-  res.json({ status: "done" });
+const createUser = async (req, res) => {
+  try {
+    const { username, password, email, gender, phone, date } = req.body;
+
+    UserDB.create(
+      { username, password, email, gender, phone, date },
+      (err, result) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(result);
+        }
+      }
+    );
+    res.json({ status: "done", data: req.body });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const findAllUser = async (req, res) => {
